13947ad1d87d495d37a1377230f80c0177674de0
838365946 <1330351736@qq.com>
2018-12-07 15:44:27 +0800

    新增简历

---
+++ src/main/java/com/example/main/controller/ResumeController.java
@@ -0,0 +1,29 @@
+package com.example.main.controller;
+
+import com.example.main.model.Message;
+import com.example.main.model.Resume;
+import com.example.main.service.ResumeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 2018/12/7.
+ */
+@RestController
+public class ResumeController {
+    @Autowired
+    private ResumeService resumeService;
+public Message SaveResume(Resume resume){
+    Message message =new Message();
+if(resumeService.SaveResume(resume).getR_id()>0){
+message.setB(true);
+message.setDes("新增简历成功");
+}else {
+    message.setB(false);
+    message.setDes("新增简历失败");
+}
+
+return message;
+}
+
+}
